b2db55abf87d0f976c10ae4bfd1a079482b8a49a
the18under <the18under@gmail.com>
2026-07-30 15:57:37 -0400

    made the button smaller

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/binding/MenuLayer.hpp>
 #include <Geode/ui/BasedButtonSprite.hpp>
+#include <Geode/ui/Layout.hpp>
 
 
 using namespace geode::prelude;
@@ -17,12 +18,16 @@ class $modify(myPauseLayer, PauseLayer) {
 		auto chatbuttonSprite = CircleButtonSprite::create(
 			CCSprite::create("chat-bubble.png"_spr),
 			CircleBaseColor::DarkAqua,
-			CircleBaseSize::Small
+			CircleBaseSize::MediumAlt
 		);
 auto button = CCMenuItemSpriteExtra::create(
-chatbuttonSprite,
-this,
-menu_selector(myPauseLayer::onChatButtonClick)
+	chatbuttonSprite,
+	this,
+	menu_selector(myPauseLayer::onChatButtonClick)
+);
+button->setLayoutOptions(
+	AxisLayoutOptions::create()
+	->setRelativeScale(0.6f)
 );
 button->setID("chat-bubble.png"_spr);
 auto menu = this->getChildByID("right-button-menu");
