e2c3e13de113fd54df97f36e8693203b5f66c998
the18under <the18under@gmail.com>
2026-07-31 01:37:44 -0400

    added a button to the editor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
-#include <Geode/binding/PauseLayer.hpp>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include "MyUtils.hpp"
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/binding/MenuLayer.hpp>
-#include <Geode/ui/BasedButtonSprite.hpp>
-#include <Geode/ui/Layout.hpp>
+#include <Geode/modify/EditorUI.hpp>
 
 
 using namespace geode::prelude;
@@ -41,17 +38,68 @@ return;
 	MotivationPopup();
 	}
 
-	
 };
 class $modify(MyPlayLayer,PlayLayer){
+	
 	void destroyPlayer(PlayerObject* player, GameObject* object) {
 		PlayLayer::destroyPlayer(player, object);
+		
 		auto x = Mod::get()->getSettingValue<int64_t>("Death-qoutes-freqency");
-if(player->m_isDead && !this->m_isPracticeMode)
-	if(m_attempts % x == 0) {
+
+		if(player->m_isDead && !this->m_isPracticeMode && m_attempts % x == 0 ) {
 		DeathNotifcation();
-	}
+}
 
 	}
 
+};
+
+class $modify(MyEditorUI, EditorUI) {
+
+	bool init(LevelEditorLayer* editorLayer) {
+		if(!EditorUI::init(editorLayer))
+			return false;
+
+		auto myUItexture = CCSprite::create("chat-bubble.png"_spr);
+	
+
+		auto btnSprite = EditorButtonSprite::create(
+			myUItexture,
+			EditorBaseColor::Gray,
+			EditorBaseSize::Normal
+		
+		);
+		
+		auto myButton = CCMenuItemSpriteExtra::create(
+			btnSprite,
+			this,
+			menu_selector(MyEditorUI::onMyButton)
+		);
+
+		
+
+		if(auto menu = this->querySelector("editor-buttons-menu")) {
+		
+			geode::log::info("menu exits");
+
+		myButton->setContentSize({40.0f, 40.0f});
+
+		if(auto btnSprite = myButton->getNormalImage()) {
+			btnSprite->setPosition({ 20.0f, 20.0f});
+		}
+
+		menu->addChild(myButton);
+		menu->updateLayout();
+		if(auto btnSprite = myButton->getNormalImage()) {
+			btnSprite->setPositionX(btnSprite->getPositionX() - 4.0f); 
+		}
+	}
+
+	return true;
+	}
+
+	void onMyButton(CCObject* sender) {
+		geode::log::info("My editor button clicked!");
+		MotivationPopup();
+	}
 };
